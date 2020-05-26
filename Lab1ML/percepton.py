48ab98cbe4288ded39f6c22c525d398b71351df3
Mariel <mariel.petra@yahoo.com>
2020-05-26 11:40:33 +0300

    Final

--- Lab1ML/percepton.py
+++ Lab1ML/percepton.py
@@ -27,7 +27,7 @@ training_inputs.append(np.array([1, 0]))
 training_inputs.append(np.array([0, 1]))
 training_inputs.append(np.array([0, 0]))
 
-labels = np.array([1, 0, 0, 0])
+labels = np.array([0, 1, 1, 1])
 
 perceptron = Perceptron(2)
 perceptron.train(training_inputs, labels)
